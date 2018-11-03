20e22d30f2de236c487c8e58a6495202422a8746
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2018-11-03 19:58:03 +0300

    Get ready for production (#7)
     - Fix `environ` dependency
     - Start server on `0.0.0.0`

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
     [org.clojure/clojure "1.10.0-beta4"]
     [org.immutant/web "2.1.10"]
     [compojure "1.6.1"]
+    [environ "1.1.0"]
     [hiccup "1.0.5"]
   ]
   :main pablito.server
@@ -11,7 +12,6 @@
   :profiles {
     :dev {
       :dependencies [
-        [environ        "1.1.0"]
         [ring/ring-mock "0.3.2"]
       ]
       :plugins [
